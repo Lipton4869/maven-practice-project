5928f6e535e24df58c3c74fa51c78b952bbadba8
Lipton4869 <takuma.soccer.pk@gmail.com>
2024-05-02 15:24:41 +0900

    初回コミット

---
+++ src/main/java/jp/co/sample/mavenpracticeproject/MavenPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package jp.co.sample.mavenpracticeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPracticeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenPracticeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/sample/mavenpracticeproject/MavenPracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.sample.mavenpracticeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPracticeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
